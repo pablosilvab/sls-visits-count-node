df54bd6c04d50b86543a2f39786ebd5aac88a630
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-07-19 23:18:43 -0400

    add jest and move package.json

--- tests/greeter.test.js
+++ tests/greeter.test.js
@@ -1,3 +1,11 @@
 'use strict';
 
 const greeter = require('../src/greeter');
+
+
+describe('sayHello', () => {
+    test('Undefined name greet', () => {
+        const greet = greeter.sayHello()
+        expect(greet).toBe('Hello World!')
+    });
+});
